f40e5fc49f4b44f73429307fc9a31a93e8122ccf
Yuriy Kis <yurakis9@gmail.com>
2021-11-10 23:12:03 +0100

    Update props view

--- PropsWindow.py
+++ PropsWindow.py
@@ -25,20 +25,31 @@ class PropsWindow(Gtk.Dialog):
         vbox_left.pack_start(service_name_label, False, True, 10)
 
         unit_details = SystemdManager.getUnitDetails(serviceName).Unit
+        service_details = SystemdManager.getUnitDetails(serviceName).Service
 
         serice_description_label = Gtk.Label(
             unit_details.Description.decode('UTF-8'), xalign=0)
         vbox_left.pack_start(serice_description_label, True, True, 0)
 
-        is_service_loaded_label = Gtk.Label(
-            "Loaded: " + unit_details.LoadState.decode('UTF-8'), xalign=0)
+        is_service_loaded_label = Gtk.Label(xalign=0)
+        is_service_loaded_label.set_markup(
+            "<b>Loaded:</b> " + unit_details.LoadState.decode('UTF-8'))
         vbox_left.pack_start(is_service_loaded_label, True, True, 0)
 
-        is_service_active_label = Gtk.Label(
-            "Active: " + unit_details.ActiveState.decode('UTF-8'), xalign=0)
-        is_service_active_label.set_justify(Gtk.Justification.LEFT)
+        fragment_path_label = Gtk.Label(xalign=0)
+        fragment_path_label.set_markup(
+            "<b>Path:</b> " + unit_details.FragmentPath.decode('UTF-8'))
+        vbox_left.pack_start(fragment_path_label, True, True, 0)
+
+        is_service_active_label = Gtk.Label(xalign=0)
+        is_service_active_label.set_markup(
+            "<b>Active:</b> " + unit_details.ActiveState.decode('UTF-8'))
         vbox_left.pack_start(is_service_active_label, True, True, 0)
 
+        main_pid_label = Gtk.Label(xalign=0)
+        main_pid_label.set_markup("<b>Main PID:</b> " +
+                                  str(service_details.MainPID))
+        vbox_left.pack_start(main_pid_label, True, True, 0)
         main_area.pack_start(vbox_left, True, True, 0)
 
         vbox_right = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
