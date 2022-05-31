498ccb36772bbb986b37e5b6f652a2f9b108c69f
Yuriy Kis <yurakis9@gmail.com>
2022-05-31 20:40:40 +0200

    Remove unnecessary import

--- gnome/start.py
+++ gnome/start.py
@@ -9,9 +9,6 @@ settings.set_property("gtk-application-prefer-dark-theme", True)
 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
-import sys
-
-sys.path.append('/home/yuriy/GTK/SystemdGUI/systemd/')
 
 
 def main():
